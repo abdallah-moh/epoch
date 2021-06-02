1317f916302c46804ca56e3138750e6d77081bc9
Rajdeep Singha <68198116+Suzie97@users.noreply.github.com>
2021-06-02 06:09:50 +0530

    Add files via upload

--- meson/post_install.py
+++ meson/post_install.py
@@ -3,8 +3,7 @@
 import os
 import subprocess
 
-install_prefix = os.environ['MESON_INSTALL_PREFIX']
-schemadir = os.path.join(install_prefix, 'share/glib-2.0/schemas')
+schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
     
 if not os.environ.get('DESTDIR'):
     print('Compiling the gsettings schemas ... ')
